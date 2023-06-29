282654db41a97cff14cba607ba9b59913190937b
Alexandr <jwchaganav@gmail.com>
2023-06-29 09:28:19 +0400

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
         float num = getFloat();
         System.out.println(num);
+        // Задача 2
+        task2();
+
     }
+
+    /**
+     * @apiNote (Задание 1) метод обрабатывает текст в дробное число
+     * @return дробное число
+     */
     public static float getFloat(){
         float result = 0;
         Scanner scanner = new Scanner(System.in);
@@ -26,6 +35,12 @@ public class Main {
         }
         return result;
     }
+
+    /**
+     * @apiNote проверка на наличие не числовых символов
+     * @param str
+     * @return логическое значение
+     */
     private static boolean contentDigit(String str){
         for (int i = 0; i < str.length(); i++) {
             if(!Character.isDigit(str.charAt(i))){
@@ -34,5 +49,25 @@ public class Main {
             }
         }
         return true;
+    }
+
+    /**
+     * @apiNote (Задание 2) Проверка на корректность кода
+     */
+    public static void task2(){
+        //Задание 2
+        int[] intArray = {1,4,6,8,4,8,8,3,34,6,67,45}; // не был объявлен массив
+        try {
+            int d = 0;
+            if (d!=0) {
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            }else {
+                System.out.println("Нельзя делить на 0");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
     }
 }
